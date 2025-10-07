b33bdea85e739b7778054d2b3703a00e5afe7abd
Serova Alina <serovaallina2005@gmail.com>
2025-10-08 00:58:49 +0300

    v

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/task6/',
+  base: '/task6/',
   plugins: [react()],
   https: true,
   server: {
     proxy: {
       '/api': {
-        target: 'https://theapptask5.somee.com',
+        target: 'https://localhost:7181',
         changeOrigin: true,
         secure: false,
       },
